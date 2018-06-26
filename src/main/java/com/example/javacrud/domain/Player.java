1bdc296fdcf88bf0cd08f37d454a409b9fdb22c7
asuforce <owata.sn@gmail.com>
2018-06-26 16:14:25 +0900

    add validation

--- src/main/java/com/example/javacrud/domain/Player.java
+++ src/main/java/com/example/javacrud/domain/Player.java
@@ -4,14 +4,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Player {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
     private String name;
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 150)
     private Integer age;
+    @Size(max = 20)
     private String team;
     private String position;
 
